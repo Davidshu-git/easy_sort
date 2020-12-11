ebdc2dfe94e6842d9995f341e12a386c9b1da747
Davidshu-git <807172378@qq.com>
2020-12-11 10:31:41 +0800

    add merge sort

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ using std::endl;
 using std::vector;
 using std::string;
 
+//快速排序经典写法
 void quick_sort(vector<int> &nums, int left, int right) {
-    if(left == right) {
+    if(left + 1 >= right) {
         return;
     }
     int first = left;
@@ -31,11 +32,38 @@ void quick_sort(vector<int> &nums, int left, int right) {
     quick_sort(nums, first + 1, right);
 }
 
+//归并排序经典写法
+void merge_sort(vector<int> &nums, int left, int right, vector<int> &temp) {
+    if(left + 1 >= right) {
+        return;
+    }
+    int middle = left + (right - left) / 2;
+    merge_sort(nums, left, middle, temp);
+    merge_sort(nums, middle, right, temp);
+    int left_start = left;
+    int right_start = middle;
+    int i = left;
+    while (left_start < middle || right_start < right)
+    {
+        if(right_start >= right || (left_start < middle 
+        && nums[left_start] <= nums[right_start])) {
+            temp[i++] = nums[left_start++];
+        } else {
+            temp[i++] = nums[right_start++];
+        }
+    }
+    for(int i = left; i < right; ++i) {
+        nums[i] = temp[i];
+    }
+}
+
+//
 int main()
 {
     vector<int> nums = {1, 3, 5, 7, 2, 6, 4, 8, 9, 2, 8, 
     7, 6, 0, 3, 5, 9, 4, 1, 0};
-    quick_sort(nums, 0, nums.size());
+    vector<int>temp(nums.size());
+    merge_sort(nums, 0, nums.size(), temp);
     for(const auto &element : nums) {
         cout << element << " ";
     }
